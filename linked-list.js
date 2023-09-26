f03da28f96a3c87106b52eed8b7b372993bf6abb
dianaloz <dlbogota2104@gmail.com>
2023-09-26 14:34:53 -0400

    Corrected exercises after running the tests, until they did not fail

--- linked-list.js
+++ linked-list.js
@@ -24,20 +24,30 @@ class LinkedList {
     let newNode = new Node(val);
     if (this.head == null) {
       this.head = newNode;
+      this.tail = this.head
     }
-    if (this.tail !== null) {
+    else {
       this.tail.next = newNode;
+      this.tail = newNode;
     }
-    this.tail = newNode;
+    this.length += 1;
   }
 
   /** unshift(val): add new value to start of list. */
 
   unshift(val) {
     let newNode = new Node(val)
-    let currentHead = this.head
-    newNode.next = currentHead;
-    this.head = newNode;
+    if (this.head == null) {
+      this.head = newNode;
+    }
+    else {
+      let currentHead = this.head
+      newNode.next = currentHead;
+      this.head = newNode;
+    }
+    if (this.length === 0) this.tail = this.head;
+
+    this.length += 1;
   }
 
   /** pop(): return & remove last item. */
@@ -45,15 +55,25 @@ class LinkedList {
   pop() {
     if (!this.head) {
       throw "error";
+
+    }
+    if (this.head === this.tail) {
+      let popNode = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length -= 1;
+      return popNode.val;
+
     }
     else {
       let currentNode = this.head;
       let popNode = this.tail;
-      while (currentNode !== this.tail) {
+      while (currentNode.next !== popNode) {
         currentNode = currentNode.next;
       }
-      currentNode.next = null;
       this.tail = currentNode;
+      this.tail.next = null;
+      this.length -= 1;
       return popNode.val;
     }
   }
@@ -64,9 +84,17 @@ class LinkedList {
     if (!this.head) {
       throw "error";
     }
+    if (this.length === 1) {
+      let shiftNode = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length -= 1;
+      return shiftNode.val;
+    }
     else {
       let shiftNode = this.head;
-      this.head = shiftNode.next
+      this.head = shiftNode.next;
+      this.length -= 1;
       return shiftNode.val;
     }
   }
@@ -74,67 +102,117 @@ class LinkedList {
   /** getAt(idx): get val at idx. */
 
   getAt(idx) {
-    let currentNode = this.head;
-    let count = 0;
-    while (count !== idx) {
-      count += 1;
-      currentNode = currentNode.next
+    if (idx >= this.length || idx < 0) {
+      throw "Error";
     }
-    return currentNode.val;
+    else {
+      let currentNode = this.head;
+      let count = 0;
+      while (count !== idx) {
+        count += 1;
+        currentNode = currentNode.next
+      }
+      return currentNode.val;
+    }
+
   }
 
   /** setAt(idx, val): set val at idx to val */
 
   setAt(idx, val) {
-    let currentNode = this.head;
-    let count = 0;
-    while (count === idx) {
-      count += 1;
-      currentNode = currentNode.next
+    if (idx >= this.length || idx < 0) {
+      throw "Error";
     }
-    currentNode.val = val;
+    else {
+      let currentNode = this.head;
+      let count = 0;
+      while (count !== idx) {
+        count += 1;
+        currentNode = currentNode.next
+      }
+      currentNode.val = val;
+    }
+
   }
 
-  /** insertAt(idx, val): add node w/val before idx. */
+  /** insertAt(idx, val): add node
+   *  w/val before idx. */
 
   insertAt(idx, val) {
-    let currentNode = this.head;
-    let count = 0;
-    let udx = idx - 1;
-    while (count < udx) {
-      count += 1;
-      currentNode = currentNode.next
+    let newNode = new Node(val);
+    if (idx > this.length || idx < 0) {
+      throw "Error, invalid index";
     }
-    currentNode.val = val;
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+      this.length += 1;
+    }
+    else if (this.length === idx) {
+      this.push(val);
+      this.length += 1;
+    }
+    else {
+      let currentNode = this.head;
+      let count = 0;
+      let udx = idx - 1;
+      while (count !== udx) {
+        count += 1;
+        currentNode = currentNode.next
+      }
+      let continuation = currentNode.next
+      currentNode.next = newNode
+      newNode.next = continuation;
+      this.length += 1;
+    }
   }
 
   /** removeAt(idx): return & remove item at idx, */
 
   removeAt(idx) {
-    let currentNode = this.head;
-    let count = 0;
-    let udx = idx - 1;
-    while (count < udx) {
-      count += 1;
-      currentNode = currentNode.next
+    if (idx >= this.length || idx < 0) {
+      throw "Error";
     }
-    let removalVal = currentNode.next;
-    currentNode.next = removalVal.next;
-    return removalVal.val;
+    if (this.length === idx - 1) {
+      this.pop()
+      this.length -= 1;
+    }
+    else if (this.length === 1) {
+      this.pop()
+    }
+    else {
+      let currentNode = this.head;
+      let count = 0;
+      let udx = idx - 1;
+      while (count !== udx) {
+        count += 1;
+        currentNode = currentNode.next
+      }
+      this.length -= 1;
+      let removalVal = currentNode.next;
+      currentNode.next = removalVal.next;
+      return removalVal.val;
+
+    }
+
   }
 
   /** average(): return an average of all values in the list */
 
   average() {
     let currentNode = this.head;
-    let count = 0;
     let allvalues = 0;
-    while (currentNode !== this.tail) {
-      currentNode = currentNode.next;
-      count += 1;
-      allvalues += currentNode.val;
+    if (this.length === 0) {
+      return 0;
     }
-    console.log(allvalues / count)
+    else {
+      while (currentNode !== this.tail) {
+        allvalues += currentNode.val;
+        currentNode = currentNode.next;
+      }
+      allvalues += this.tail.val;
+      return (allvalues / this.length)
+    }
   }
 }
 
